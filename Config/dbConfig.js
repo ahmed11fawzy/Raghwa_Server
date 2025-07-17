e79728ec9ad1458f40c85b3ba678ec968e39b145
Your Name <you@example.com>
2025-07-17 13:26:30 +0300

    refactor

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -1,7 +1,6 @@
 const { Sequelize } = require("sequelize");
 const dontenv = require("dotenv");
 
-console.log(process.env.NODE_ENV);
 const sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
   host: process.env.DATABASE_HOST,
   dialect: "mysql",
