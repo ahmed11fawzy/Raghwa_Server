af0c4ac15fe3aaac54b9829c79dbd51ea7fc9882
reus <af6394158@gmail.com>
2025-07-19 01:36:47 +0300

    create Service controller

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,4 +1,5 @@
 const { Service, Product, ProductService } = require("../Model");
+const { findProductsWithCost } = require("../utils/findProductsWithCost");
 const catchAsync = require("../utils/catchAsync");
 const getAllServices = async (req, res, next) => {
   const services = await Service.findAll({
@@ -18,7 +19,64 @@ const getAllServices = async (req, res, next) => {
 };
 
 const createService = catchAsync(async (req, res, next) => {
-  const service = await Service.create(req.body);
+  const { name, duration, type, serviceCode, description, profitMarginPercent, products } = req.body;
+
+  const serviceData = { name, duration, type, serviceCode, description, profitMarginPercent };
+  // TODO 1. Validate and get products
+
+  const consumedProducts = await findProductsWithCost(products);
+
+  // TODO 2. Create Product Cost Map
+
+  const productCostMap = new Map();
+  consumedProducts.forEach((product) => {
+    productCostMap.set(product.id, product.cost);
+  });
+
+  // TODO 3. Calculate total cost
+  let totalCost = 0;
+  const productServiceData = [];
+
+  for (const consumedProduct of products) {
+    const { productId, quantity } = consumedProduct;
+    const unitCost = productCostMap.get(productId);
+    const itemTotalCost = parseFloat(quantity) * parseFloat(unitCost);
+
+    totalCost += itemTotalCost;
+
+    productServiceData.push({
+      productId,
+      quantity,
+      unitCostAtTime: unitCost,
+      totalCost: itemTotalCost,
+    });
+  }
+
+  // TODO 4. Calculate final price with margin
+  const profitMargin = parseFloat(serviceData.profitMarginPercent || 20);
+  const finalPrice = totalCost * (1 + profitMargin / 100);
+
+  // TODO 5. Create service
+  const service = await Service.create(
+    {
+      ...serviceData,
+      totalCost: totalCost.toFixed(2),
+      profitMarginPercent: profitMargin,
+      finalPrice: finalPrice.toFixed(2),
+    },
+    { transaction }
+  );
+
+  // TODO 6. Create product-service relationships
+  const productServicesWithServiceId = productServiceData.map((ps) => ({
+    ...ps,
+    serviceId: service.id,
+  }));
+
+  await ProductService.bulkCreate(productServicesWithServiceId, { transaction });
+
+  await transaction.commit();
+
   res.status(201).json({
     status: "success",
     data: {
