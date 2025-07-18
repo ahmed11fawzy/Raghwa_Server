5418f7f88700e1653f1e26d14ce1e0c5e687379e
Your Name <you@example.com>
2025-07-18 22:47:05 +0300

    updated create company controller and company routes

--- routes/branchesRoutes.js
+++ routes/branchesRoutes.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const branchController = require("../controllers/branchesController");
-
-// Create a new branch
-router.post("/", branchController.createBranch);
-
-// Get all branches
-router.get("/", branchController.getAllBranches);
-
-// Get a single branch by ID
-router.get("/:id", branchController.getBranchById);
-
-// Update a branch
-router.patch("/:id", branchController.updateBranch);
-
-// Delete a branch
-router.delete("/:id", branchController.deleteBranch);
+router.route("/").post(branchController.createBranch).get(branchController.getAllBranches);
+router
+  .route("/:id")
+  .get(branchController.getBranchById)
+  .patch(branchController.updateBranch)
+  .delete(branchController.deleteBranch);
 
 module.exports = router;
