49d98fe8370bb7e3db708eb819cdbba5e6f3419e
reus <af6394158@gmail.com>
2025-07-19 01:36:47 +0300

    create Service controller

--- Model/productModel.js
+++ Model/productModel.js
@@ -1,41 +1,54 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../Config/dbConfig");
-const Product = sequelize.define("Product", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Product = sequelize.define(
+  "Product",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    code: {
+      type: DataTypes.STRING(10),
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  price: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-  image: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  isActive: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: true,
-  },
-  type: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  code: {
-    type: DataTypes.STRING(10),
-    defaultValue: DataTypes.UUIDV4,
-    allowNull: false,
-  },
-});
+  {
+    indexes: [
+      {
+        fields: ["isActive"],
+      },
+      {
+        fields: ["name"],
+      },
+    ],
+  }
+);
 
 module.exports = Product;
