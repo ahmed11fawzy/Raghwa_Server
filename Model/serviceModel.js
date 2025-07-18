d7a82374416a85da1fa13fd480a04765d1aa61a3
reus <af6394158@gmail.com>
2025-07-19 01:49:49 +0300

    solve confilict bug in service model

--- Model/serviceModel.js
+++ Model/serviceModel.js
@@ -1,62 +1,55 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../Config/dbConfig");
 
-const Service = sequelize.define(
-  "Service",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    serviceCode: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    type: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    isActive: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    },
-    targetCar: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    Image: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    duration: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    profitMarginPercent: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    totalCost: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    finalPrice: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
+const Service = sequelize.define("Service", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  serviceCode: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+  targetCar: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  Image: {
+    type: DataTypes.STRING,
+    allowNull: true,
   },
   duration: {
-    type: DataTypes.INTEGER,
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  profitMarginPercent: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  totalCost: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  finalPrice: {
+    type: DataTypes.FLOAT,
     allowNull: false,
   },
 });
