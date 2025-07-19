255adf272757313d823e00c27c995b0f3d6af37b
Your Name <you@example.com>
2025-07-19 08:54:32 +0300

    implemented file uploading and used cors

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -4,7 +4,10 @@ const companyController = require("../controllers/companyController");
 const branchController = require("../controllers/branchesController");
 
 router.route("/").post(companyController.createCompany).get(companyController.getAllCompanies);
-router.route("/:id/branches").get(companyController.getCompanyBranches).post(companyController.createBranch);
+router
+  .route("/:id/branches")
+  .get(companyController.getCompanyBranches)
+  .post(companyController.uploadBranchIcon, companyController.createBranch);
 router
   .route("/:id")
   .get(companyController.getCompanyById)
